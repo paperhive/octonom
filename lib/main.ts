034374c5a37b7b053adbda2244b9975de473b8f2
André Gaul <andre@gaul.io>
2017-07-08 21:56:14 +0200

    main: add ModelArray

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,4 @@
 export { Collection, ICollectionOptions } from './collection';
 export { Model } from './model';
+export { ModelArray } from './model-array';
 export { MongoCollection } from './mongo-collection';
