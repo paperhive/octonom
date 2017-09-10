b71ba8acdae1cb69f5b1eb3707a7b8b0280a0cb6
André Gaul <andre@gaul.io>
2017-09-10 18:20:29 +0200

    main: expose Hook

--- packages/octonom/lib/main.ts
+++ packages/octonom/lib/main.ts
@@ -3,6 +3,6 @@ import * as utils from './utils';
 export { ArrayCollection } from './array-collection';
 export { Collection, ICollectionOptions } from './collection';
 export { ValidationError } from './errors';
-export { IModelConstructor, Model, Property } from './model';
+export { Hook, IModelConstructor, Model, Property } from './model';
 export { ModelArray } from './model-array';
 export { utils };
