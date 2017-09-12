4b13360c1836c0886ed622bbaac03d919b7cbc94
André Gaul <andre@gaul.io>
2017-09-12 10:04:53 +0200

    octonom-timestamp: fix exports

--- packages/octonom-timestamp/lib/main.ts
+++ packages/octonom-timestamp/lib/main.ts
@@ -1,1 +1,5 @@
-export { Timestamp } from './timestamp';
+// TODO: remove export when
+//       https://github.com/Microsoft/TypeScript/issues/9944 and/or
+//       https://github.com/Microsoft/TypeScript/issues/5711
+//       are solved in a published Typescript version
+export { ITimestampStatic, Timestamp } from './timestamp';
