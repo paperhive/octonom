fef3a49b093921874a20421cb3fd92c7c0d0fbf3
André Gaul <andre@gaul.io>
2017-09-18 09:22:17 +0200

    test: add model that has a property for each schema

---
+++ packages/octonom/test/data/models/all.ts
@@ -0,0 +1,44 @@
+import { ArrayCollection, Model, Property, Schema } from '../../../lib/main';
+
+export class NestedModel extends Model {
+  @Property.String()
+  public id: string;
+
+  @Property.String()
+  public name: string;
+}
+
+export const nestedCollection = new ArrayCollection<NestedModel>(NestedModel, {modelIdField: 'id'});
+
+export class AllModel extends Model {
+  @Property.Any()
+  public any: any;
+
+  @Property.Array({elementSchema: new Schema.Boolean()})
+  public array: boolean[];
+
+  @Property.Boolean()
+  public boolean: boolean;
+
+  @Property.Date()
+  public date: Date;
+
+  @Property.Model({model: NestedModel})
+  public model: NestedModel;
+
+  @Property.Number()
+  public number: number;
+
+  @Property.Object({schema: {enabled: new Schema.Boolean()}})
+  public object: {enabled: boolean};
+
+  @Property.Reference({collection: () => nestedCollection})
+  public reference: string | NestedModel;
+
+  @Property.String()
+  public string: string;
+
+  constructor(data?: Partial<AllModel>) {
+    super(data);
+  }
+}
