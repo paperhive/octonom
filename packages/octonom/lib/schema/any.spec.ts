8b527e32ae84fa4a0c12dee1dc041de5d126b61c
André Gaul <andre@gaul.io>
2017-09-30 20:12:42 +0200

    AnySchema -> OctoAny

--- packages/octonom/lib/schema/any.spec.ts
+++ packages/octonom/lib/schema/any.spec.ts
@@ -1,33 +1,30 @@
 import { ValidationError } from '../errors';
-import { Model } from '../model';
-import { AnySchema } from './any';
+import { OctoAny, OctoAnyFactory } from './any';
 
 describe('AnySchema', () => {
   describe('sanitize()', () => {
     it('should return a default value', () => {
-      const schema = new AnySchema({default: {foo: 'bar'}});
-      expect(schema.sanitize(undefined, ['key'], {} as Model, {defaults: true}))
+      expect(OctoAny.sanitize(undefined, {default: {foo: 'bar'}}, {defaults: true}))
         .to.eql({foo: 'bar'});
     });
 
     it('should return a default value from a function', () => {
-      const schema = new AnySchema({default: () => 'foo'});
-      expect(schema.sanitize(undefined, ['key'], {} as Model, {defaults: true}))
+      expect(OctoAny.sanitize(undefined, {default: () => 'foo'}, {defaults: true}))
         .to.eql('foo');
     });
 
     it('should return any value', () => {
-      const schema = new AnySchema();
       const obj = {foo: 'bar'};
-      expect(schema.sanitize(obj, ['key'], {} as Model)).to.equal(obj);
+      expect(OctoAny.sanitize(obj)).to.equal(obj);
     });
   });
 
   describe('toObject()', () => {
     it('should return any value cloned', () => {
-      const schema = new AnySchema();
+      const schema = OctoAnyFactory.create();
       const obj = {foo: 'bar'};
-      const result = schema.toObject(obj);
+      const octoValue = schema(obj);
+      const result = octoValue.toObject();
       expect(result).to.not.equal(obj);
       expect(result).to.eql(obj);
     });
@@ -35,30 +32,30 @@ describe('AnySchema', () => {
 
   describe('validate()', () => {
     it('should throw a ValidationError if required but undefined', async () => {
-      const schema = new AnySchema({required: true});
-      await expect(schema.validate(undefined, ['key'], {} as Model))
+      const schema = OctoAnyFactory.create({required: true});
+      await expect(schema(undefined).validate())
         .to.be.rejectedWith(ValidationError, 'Required value is undefined.');
     });
 
     it('should run custom validator', async () => {
-      const schema = new AnySchema({
-        validate: async value => {
-          if (value === true) {
+      const schema = OctoAnyFactory.create({
+        validate: async octoValue => {
+          if (octoValue.value === true) {
             throw new ValidationError('true is not allowed.');
           }
         },
       });
-      await schema.validate(false, ['key'], {} as Model);
-      await expect(schema.validate(true, ['key'], {} as Model))
+      await schema(false).validate();
+      await expect(schema(true).validate())
         .to.be.rejectedWith(ValidationError, 'true is not allowed.');
     });
 
     it('should validate anything', async () => {
-      const schema = new AnySchema();
-      await schema.validate(undefined, ['key'], {} as Model);
-      await schema.validate(true, ['key'], {} as Model);
-      await schema.validate(['foo'], ['key'], {} as Model);
-      await schema.validate({foo: 'bar'}, ['key'], {} as Model);
+      const schema = OctoAnyFactory.create();
+      await schema(undefined).validate();
+      await schema(true).validate();
+      await schema(['foo']).validate();
+      await schema({foo: 'bar'}).validate();
     });
   });
 });
