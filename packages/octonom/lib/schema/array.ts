a2f5abf04c48c14d2270ce64334d0e2f6eed41d5
André Gaul <andre@gaul.io>
2017-10-01 16:06:51 +0200

    add tests for array proxy

--- packages/octonom/lib/schema/array.ts
+++ packages/octonom/lib/schema/array.ts
@@ -14,7 +14,7 @@ export interface IArrayOptions<T = any> extends ISchemaOptions<OctoArray<T>> {
 }
 
 export class OctoArray<T = any> extends OctoValue<T[]> {
-  protected octoValues: Array<OctoValue<any>>;
+  public octoValues: Array<OctoValue<any>>;
 
   constructor(value: any, public schemaOptions: IArrayOptions<T>, sanitizeOptions: ISanitizeOptions) {
     super(value, schemaOptions, sanitizeOptions);
@@ -80,6 +80,194 @@ export class OctoArray<T = any> extends OctoValue<T[]> {
     await super.validate();
   }
 
+  protected getOctoValuesProxy() {
+    return new Proxy(this.octoValues, {
+      get: (target, key, receiver) => {
+        switch (key) {
+          case 'copyWithin':
+            throw new Error('not yet implemented');
+          case 'fill':
+            throw new Error('not yet implemented');
+          case 'pop':
+            return () => {
+              const clone = target.map(el => el.value);
+              clone.pop();
+              this.beforeChange([], clone, this);
+
+              const removedOctoValue = target.pop();
+              delete removedOctoValue.parent;
+
+              this.afterChange([], clone, this);
+
+              return removedOctoValue.value;
+            };
+          case 'push':
+            return (...args: any[]) => {
+              const clone = target.map(el => el.value);
+              clone.push(...args);
+              this.beforeChange([], clone, this);
+
+              const newOctoValues = args.map((arg, index) => {
+                return this.schemaOptions.elementSchema(
+                  arg,
+                  {parent: {octoValue: this, path: target.length + index}},
+                );
+              });
+              const result = target.push(...newOctoValues);
+
+              this.afterChange([], clone, this);
+
+              return result;
+            };
+          case 'reverse':
+            return () => {
+              const clone = target.map(el => el.value);
+              clone.reverse();
+              this.beforeChange([], clone, this);
+
+              target.reverse();
+              target.forEach((octoValue, index) => octoValue.parent.path = index);
+
+              this.afterChange([], clone, this);
+
+              return receiver;
+            };
+          case 'sort':
+            return (compare: (a, b) => number) => {
+              const mapped = target.map((octoValue, index) => ({octoValue, index}));
+              mapped.sort((a, b) => {
+                if (compare) {
+                  return compare(a.octoValue.value, b.octoValue.value);
+                } else {
+                  if (a.octoValue.value < b.octoValue.value) {
+                    return -1;
+                  }
+
+                  if (a.octoValue.value > b.octoValue.value) {
+                    return 1;
+                  }
+
+                  return 0;
+                }
+              });
+              const clone = mapped.map(el => el.octoValue.value);
+
+              this.beforeChange([], clone, this);
+
+              target.splice(0, target.length, ...mapped.map((el, index) => {
+                el.octoValue.parent.path = index;
+                return el.octoValue;
+              }));
+
+              this.afterChange([], clone, this);
+
+              return receiver;
+            };
+          case 'splice':
+            return (start, deleteCount, ...args: any[]) => {
+              if (deleteCount === undefined || deleteCount > target.length - start) {
+                deleteCount = target.length - start;
+              }
+              const clone = target.map(el => el.value);
+              clone.splice(start, deleteCount, ...args);
+              this.beforeChange([], clone, this);
+
+              const newOctoValues = args.map((arg, index) => {
+                return this.schemaOptions.elementSchema(
+                  arg,
+                  {parent: {octoValue: this, path: start + index}},
+                );
+              });
+              const removedOctoValues = target.splice(start, deleteCount, ...newOctoValues);
+              removedOctoValues.forEach(octoValue => delete octoValue.parent);
+              target.slice(start + newOctoValues.length).forEach((octoValue, index) => {
+                octoValue.parent.path = start + newOctoValues.length + index;
+              });
+
+              this.afterChange([], clone, this);
+
+              return removedOctoValues.map(el => el.value);
+            };
+          case 'toString':
+            return () => target.map(el => el.value).toString();
+          case 'unshift':
+            return (...args: any[]) => {
+              const clone = target.map(el => el.value);
+              clone.unshift(...args);
+              this.beforeChange([], clone, this);
+
+              const newOctoValues = args.map((arg, index) => {
+                return this.schemaOptions.elementSchema(
+                  arg,
+                  {parent: {octoValue: this, path: index}},
+                );
+              });
+
+              const result = target.unshift(...newOctoValues);
+              target.slice(newOctoValues.length).forEach((octoValue, index) => {
+                octoValue.parent.path = newOctoValues.length + index;
+              });
+
+              this.afterChange([], clone, this);
+
+              return result;
+            };
+        }
+
+        if (typeof key === 'number' || typeof key === 'string' && /^\d$/.test(key)) {
+          const numKey = typeof key === 'number' ? key : parseInt(key, 10);
+          const octoValue = this.octoValues[numKey];
+          return octoValue ? octoValue.value : undefined;
+        }
+
+        return target[key];
+      },
+      set: (target, key, value, receiver) => {
+        if (typeof key === 'number' || typeof key === 'string' && /^\d$/.test(key)) {
+          const numKey = typeof key === 'number' ? key : parseInt(key, 10);
+          const oldOctoValue = target[numKey];
+
+          this.beforeChange([numKey], value, oldOctoValue);
+
+          const newOctoValue = this.schemaOptions.elementSchema(
+            value,
+            {parent: {octoValue: this, path: numKey}},
+          );
+
+          if (oldOctoValue) {
+            delete oldOctoValue.parent;
+          }
+
+          target[numKey] = newOctoValue;
+
+          this.afterChange([numKey], value, newOctoValue);
+        } else {
+          target[key] = value;
+        }
+        return true;
+      },
+      deleteProperty: (target, key) => {
+        if (typeof key === 'number' || typeof key === 'string' && /^\d$/.test(key)) {
+          const numKey = typeof key === 'number' ? key : parseInt(key, 10);
+          const oldOctoValue = this.octoValues[numKey];
+
+          this.beforeChange([numKey], undefined, oldOctoValue);
+
+          if (oldOctoValue) {
+            delete oldOctoValue.parent;
+          }
+
+          delete target[numKey];
+
+          this.afterChange([numKey], undefined, undefined);
+        } else {
+          delete target[key];
+        }
+        return true;
+      },
+    }) as any[];
+  }
+
   protected sanitize(value: any, sanitizeOptions: ISanitizeOptions) {
     this.value = undefined;
     this.octoValues = undefined;
@@ -104,186 +292,7 @@ export class OctoArray<T = any> extends OctoValue<T[]> {
       );
     });
 
-    const array = this.octoValues.map(element => element.value);
-
-    return new Proxy(array, {
-      get: (target, key, receiver) => {
-        switch (key) {
-          case 'copyWithin':
-            throw new Error('not yet implemented');
-          case 'fill':
-            throw new Error('not yet implemented');
-          case 'pop':
-            return () => {
-              const clone = target.slice();
-              clone.pop();
-              this.beforeChange([], clone, this);
-
-              const removedOctoValue = this.octoValues.pop();
-              delete removedOctoValue.parent;
-              const result = target.pop();
-
-              this.afterChange([], clone, this);
-
-              return result;
-            };
-          case 'push':
-            return (...args: any[]) => {
-              const clone = target.slice();
-              clone.push(...args);
-              this.beforeChange([], clone, this);
-
-              const newOctoValues = args.map((arg, index) => {
-                return this.schemaOptions.elementSchema(
-                  arg,
-                  {parent: {octoValue: this, path: this.octoValues.length + index}},
-                );
-              });
-              this.octoValues.push(...newOctoValues);
-              const result = target.push(...newOctoValues.map(octoValue => octoValue.value));
-
-              this.afterChange([], clone, this);
-
-              return result;
-            };
-          case 'reverse':
-            return () => {
-              const clone = target.slice();
-              clone.reverse();
-              this.beforeChange([], clone, this);
-
-              this.octoValues.reverse();
-              this.octoValues.forEach((octoValue, index) => {
-                octoValue.parent.path = index;
-              });
-              const result = target.reverse();
-
-              this.beforeChange([], clone, this);
-
-              return this.value;
-            };
-          case 'sort':
-            return (compare: (a, b) => number) => {
-              const mapped = target.map((element, index) => ({element, index}));
-              mapped.sort((a, b) => {
-                if (compare) {
-                  return compare(a.element, b.element);
-                } else {
-                  if (a.element < b.element) {
-                    return -1;
-                  }
-
-                  if (a.element > b.element) {
-                    return 1;
-                  }
-
-                  return 0;
-                }
-              });
-              const clone = mapped.map(el => el.element);
-
-              this.beforeChange([], clone, this);
-
-              this.octoValues.splice(0, this.octoValues.length, ...mapped.map((el, index) => {
-                const octoValue = this.octoValues[el.index];
-                octoValue.parent.path = index;
-                return octoValue;
-              }));
-              target.splice(0, target.length, ...this.octoValues.map(octoValue => octoValue.value));
-
-              this.afterChange([], clone, this);
-            };
-          case 'splice':
-            return (start, deleteCount, ...args: any[]) => {
-              const clone = target.slice();
-              clone.splice(start, deleteCount, ...args);
-              this.beforeChange([], clone, this);
-
-              const newOctoValues = args.map((arg, index) => {
-                return this.schemaOptions.elementSchema(
-                  arg,
-                  {parent: {octoValue: this, path: start + index}},
-                );
-              });
-              const removedOctoValues = this.octoValues.splice(start, deleteCount, ...newOctoValues);
-              removedOctoValues.forEach(octoValue => delete octoValue.parent);
-              this.octoValues.slice(start + deleteCount).forEach((octoValue, index) => {
-                octoValue.parent.path = start + deleteCount + index;
-              });
-              const result = target.splice(start, deleteCount, ...newOctoValues.map(octoValue => octoValue.value));
-
-              this.afterChange([], clone, this);
-              return result;
-            };
-          case 'unshift':
-            return (...args: any[]) => {
-              const clone = target.slice();
-              clone.unshift(...args);
-              this.beforeChange([], clone, this);
-              const newOctoValues = args.map((arg, index) => {
-                return this.schemaOptions.elementSchema(
-                  arg,
-                  {parent: {octoValue: this, path: index}},
-                );
-              });
-              this.octoValues.unshift(...newOctoValues);
-              this.octoValues.slice(newOctoValues.length).forEach((octoValue, index) => {
-                octoValue.parent.path = newOctoValues.length + index;
-              });
-              const result = target.unshift(...newOctoValues.map(octoValue => octoValue.value));
-              this.afterChange([], clone, this);
-              return result;
-            };
-        }
-
-        return target[key];
-      },
-      set: (target, key, rawValue, receiver) => {
-        if (typeof key === 'number' || typeof key === 'string' && /^\d$/.test(key)) {
-          const numKey = typeof key === 'number' ? key : parseInt(key, 10);
-          const oldOctoValue = this.octoValues[numKey];
-
-          this.beforeChange([numKey], rawValue, oldOctoValue);
-
-          const newOctoValue = this.schemaOptions.elementSchema(
-            rawValue,
-            {parent: {octoValue: this, path: numKey}},
-          );
-
-          if (oldOctoValue) {
-            delete oldOctoValue.parent;
-          }
-
-          this.octoValues[numKey] = newOctoValue;
-          target[numKey] = newOctoValue.value;
-
-          this.afterChange([numKey], rawValue, newOctoValue);
-        } else {
-          target[key] = value;
-        }
-        return true;
-      },
-      deleteProperty: (target, key) => {
-        if (typeof key === 'number' || typeof key === 'string' && /^\d$/.test(key)) {
-          const numKey = typeof key === 'number' ? key : parseInt(key, 10);
-          const oldOctoValue = this.octoValues[numKey];
-
-          this.beforeChange([numKey], undefined, oldOctoValue);
-
-          if (oldOctoValue) {
-            delete oldOctoValue.parent;
-          }
-
-          delete this.octoValues[numKey];
-          delete target[numKey];
-
-          this.afterChange([numKey], undefined, undefined);
-        } else {
-          delete target[key];
-        }
-        return true;
-      },
-    });
+    return this.getOctoValuesProxy();
   }
 }
 
