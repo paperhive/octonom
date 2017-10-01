17c73f5b0c1986708fe36dd77b8b4d5710570c72
André Gaul <andre@gaul.io>
2017-10-02 01:28:59 +0200

    value: refactor for hooks handling

--- packages/octonom/lib/schema/value.ts
+++ packages/octonom/lib/schema/value.ts
@@ -1,8 +1,8 @@
 import { ValidationError } from '../errors';
 
-export interface ISchemaOptions<TOctoValue> {
+export interface ISchemaOptions<TOctoValue extends OctoValue<any>> {
   required?: boolean;
-  validate?(schema: TOctoValue): Promise<void>;
+  validate?(octoValue: TOctoValue): Promise<void>;
 }
 
 export type PopulateReference = IPopulateMap | true;
@@ -27,13 +27,18 @@ export interface IToObjectOptions {
 
 export type Path = Array<string | number>;
 
-export interface IParentValue<T = any> {
-  beforeChange(path: Path, rawValue: any, oldOctoValue: OctoValue<T>);
-  afterChange(path: Path, rawValue: any, newOctoValue: OctoValue<T>);
+export interface IOctoInstance<T = any> {
+  parent?: IParent;
+  value: T;
+}
+
+export interface IOctoParentInstance<T = any> extends IOctoInstance<T> {
+  beforeChange(path: Path, rawValue: any, oldInstance: IOctoInstance<T>);
+  afterChange(path: Path, rawValue: any, newInstance: IOctoInstance<T>);
 }
 
 export interface IParent<T = any> {
-  octoValue: OctoValue<T> & IParentValue<any>;
+  instance: IOctoParentInstance<any>;
   path: string | number;
 }
 
@@ -48,7 +53,7 @@ export interface IOctoValueConstructor<T = any, TOctoValue extends OctoValue<any
  *  additional metadata. For example, an OctoValue knows if it has a parent
  *  (property 'parent') which is useful for hooks and error messages.
  */
-export abstract class OctoValue<T> {
+export abstract class OctoValue<T> implements IOctoInstance<T> {
   public value: T;
   public parent: IParent;
 
@@ -61,7 +66,10 @@ export abstract class OctoValue<T> {
     this.value = this.sanitize(value, sanitizeOptions);
   }
 
-  /** Populate a value (possibly nested) */
+  /** Populate a value (possibly nested).
+   *  Note: do *not* the value property of the current instance inside populate().
+   *        This is the responsibility of the parent OctoValue.
+   */
   public async populate(populateReference: PopulateReference): Promise<T> {
     throw new Error(`${this.constructor.name} is not populateable.`);
   }
@@ -110,6 +118,10 @@ export class OctoFactory<
       return new this.octoValueClass(value, schemaOptions, sanitizeOptions) as TOctoValue;
     };
   }
+}
+
+export interface IOctoValueMap {
+  [key: string]: OctoValue<any>;
 }
 
 export interface ISchema {
