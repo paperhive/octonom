1927188a411b3a5b22df25ba8b8e6853cdd72a92
André Gaul <andre@gaul.io>
2017-10-04 00:56:07 +0200

    move StringSchema to object-based interface

--- packages/octonom/lib/schema/string.spec.ts
+++ packages/octonom/lib/schema/string.spec.ts
@@ -1,84 +1,131 @@
 import { SanitizationError, ValidationError } from '../errors';
-import { OctoString, StringSchema } from './string';
+import { StringSchema } from './string';
+import { IParent } from './value';
 
-describe('MetaString', () => {
+describe('StringSchema', () => {
+  const parent: IParent = {
+    instance: {} as any,
+    path: 'string',
+  };
+
   describe('constructor()', () => {
-    it('should throw a SanitizationError if value is not a string', () => {
-      expect(() => new OctoString(42))
-        .to.throw(SanitizationError, 'Value is not a string');
+    it('should be instantiatable without options', () => {
+      const schema = new StringSchema();
+      expect(schema).to.have.property('options').that.eql({});
     });
 
-    it('should return undefined', () => {
-      expect(new OctoString(undefined)).to.contain({value: undefined});
+    it('should be instantiatable with options', () => {
+      const schema = new StringSchema({min: 5});
+      expect(schema).to.have.property('options').that.eql({min: 5});
+    });
+  });
+
+  describe('create()', () => {
+    it('should throw if value is not a string', () => {
+      expect(() => new StringSchema().create(42, {parent}))
+        .to.throw(SanitizationError, 'Value is not a string.')
+        .which.has.property('parent', parent);
     });
 
-    it('should return a string', () => {
-      expect(new OctoString('bar')).to.contain({value: 'bar'});
+    it('should return a string if a string is provided (without sanitizeOptions)', () => {
+      expect(new StringSchema().create('bar')).to.eql({value: 'bar', parent: undefined});
     });
 
-    it('should return a default value', () => {
-      expect(new OctoString(undefined, {default: 'bar'}, {defaults: true}))
-        .to.contain({value: 'bar'});
+    it('should return a string if a string is provided', () => {
+      expect(new StringSchema().create('bar', {parent})).to.eql({value: 'bar', parent});
+    });
+
+    it('should return undefined if undefined is provided', () => {
+      expect(new StringSchema().create(undefined)).to.equal(undefined);
+    });
+
+    it('should return a default value if undefined is provided and defaults is enabled', () => {
+      expect(new StringSchema({default: 'bar'}).create(undefined, {defaults: true, parent}))
+        .to.eql({value: 'bar', parent});
     });
 
     it('should return a default value from a function', () => {
-      expect(new OctoString(undefined, {default: () => 'bar'}, {defaults: true}))
-        .to.contain({value: 'bar'});
+      expect(new StringSchema({default: () => 'bar'}).create(undefined, {defaults: true, parent}))
+        .to.eql({value: 'bar', parent});
     });
   });
 
   describe('validate()', () => {
-    it('should throw a ValidationError if required but undefined', async () => {
-      const schema = new StringSchema({required: true});
-      await expect(schema.create(undefined).validate())
-        .to.be.rejectedWith(ValidationError, 'Required value is undefined.');
+    function testValidation(validationPromise: Promise<any>, expectedMsg: string, expectedParent: IParent) {
+      return expect(validationPromise).to.be.rejectedWith(ValidationError, expectedMsg)
+        .and.eventually.have.property('parent', expectedParent);
+    }
+
+    it('should throw if value is undefined but required', async () => {
+      await testValidation(
+        new StringSchema({required: true}).validate({value: undefined, parent}),
+        'Required value is undefined.',
+        parent,
+      );
+    });
+
+    it('should throw if value is not a string', async () => {
+      await testValidation(
+        new StringSchema().validate({value: 42 as any, parent}),
+        'Value is not a string.',
+        parent,
+      );
     });
 
     it('should throw if value is not in enum', async () => {
-      const schema = new StringSchema({enum: ['foo', 'bar']});
-      await expect(schema.create('baz').validate())
-        .to.be.rejectedWith(ValidationError, 'String not in enum: foo, bar.');
+      await testValidation(
+        new StringSchema({enum: ['foo', 'bar']}).validate({value: 'baz', parent}),
+        'String not in enum: foo, bar.',
+        parent,
+      );
     });
 
     it('should throw if value is shorter than min', async () => {
-      const schema = new StringSchema({min: 4});
-      await expect(schema.create('foo').validate())
-        .to.be.rejectedWith(ValidationError, 'String must not have less than 4 characters');
+      await testValidation(
+        new StringSchema({min: 4}).validate({value: 'foo', parent}),
+        'String must not have less than 4 characters',
+        parent,
+      );
     });
 
     it('should throw if value is longer than max', async () => {
-      const schema = new StringSchema({max: 5});
-      await expect(schema.create('foobar').validate())
-        .to.be.rejectedWith(ValidationError, 'String must not have more than 5 characters');
+      await testValidation(
+        new StringSchema({max: 5}).validate({value: 'foobar', parent}),
+        'String must not have more than 5 characters',
+        parent,
+      );
     });
 
     it('should throw if value does not match regex', async () => {
-      const schema = new StringSchema({regex: /foo/});
-      await expect(schema.create('bar').validate())
-        .to.be.rejectedWith(ValidationError, 'String does not match regex.');
+      await testValidation(
+        new StringSchema({regex: /foo/}).validate({value: 'bar', parent}),
+        'String does not match regex.',
+        parent,
+      );
     });
 
     it('should run custom validator', async () => {
       const schema = new StringSchema({
-        validate: async octoValue => {
-          if (octoValue.value === 'foo') {
+        validate: async instance => {
+          if (instance.value === 'foo') {
             throw new ValidationError('foo is not allowed.');
           }
         },
       });
-      await schema.create('bar', {}).validate();
-      await expect(schema.create('foo').validate())
-        .to.be.rejectedWith(ValidationError, 'foo is not allowed.');
+      await schema.validate({value: 'bar', parent});
+      await testValidation(
+        schema.validate({value: 'foo', parent}),
+        'foo is not allowed.',
+        parent,
+      );
     });
 
     it('should validate undefined', async () => {
-      const schema = new StringSchema();
-      await schema.create(undefined).validate();
+      await new StringSchema().validate({value: undefined, parent});
     });
 
     it('should validate a string', async () => {
-      const schema = new StringSchema();
-      await schema.create('foo').validate();
+      await new StringSchema().validate({value: 'foo'});
     });
   });
 });
