b21c795086d1ba6c62bb4b288194f1861cfbb9b4
André Gaul <andre@gaul.io>
2017-10-04 03:11:52 +0200

    adapt object to new schema

--- packages/octonom/lib/schema/object.spec.ts
+++ packages/octonom/lib/schema/object.spec.ts
@@ -3,10 +3,10 @@ import { spy } from 'sinon';
 import { ArrayCollection } from '../array-collection';
 import { SanitizationError, ValidationError } from '../errors';
 import { Model } from '../model';
-import { ObjectSchema, OctoObject, populateObject, setObject, toObject, validateObject } from './object';
+import { ObjectSchema, setObject, toObject, validateObject } from './object';
 import { ReferenceSchema } from './reference';
-import { OctoString, StringSchema } from './string';
-import { IOctoParentInstance, IOctoValueMap, ISchemaMap } from './value';
+import { ISchemaParentInstance, SchemaInstanceMap, SchemaMap } from './schema';
+import { StringSchema } from './string';
 
 /*
 class TestModel extends Model {
@@ -17,146 +17,164 @@ TestModel.setSchema('id', new StringSchema());
 const collection = new ArrayCollection<TestModel>(TestModel, {modelIdField: 'id'});
 const instance = new  TestModel({id: '0xACAB'});
 collection.insert(instance);
-
-describe('populateObject()', () => {
-  const schemaMap = {
-    foo: new ReferenceSchema({collection: () => collection}),
-    bar: new StringSchema(),
-  };
-
-  it('should throw if populateReference is not an object', async () => {
-    await expect(populateObject(schemaMap, {foo: '0xACAB', bar: 'baz'}, true))
-      .to.be.rejectedWith(Error, 'populateReference must be an object.');
-  });
-
-  it('should throw if a key is not populatable', async () => {
-    await expect(populateObject(schemaMap, {foo: '0xACAB', bar: 'baz'}, {bar: true}))
-      .to.be.rejectedWith(Error, 'Key bar is not populatable.');
-  });
-
-  it('should ignore a key if it\'s undefined', async () => {
-    const obj = {bar: 'test'};
-    const result = await populateObject(schemaMap, obj, {foo: true});
-    expect(result).to.not.equal(obj);
-    expect(result).to.eql({bar: 'test'});
-  });
-
-  it('should populate a key', async () => {
-    const obj = {foo: '0xACAB', bar: 'test'};
-    const result = await populateObject(schemaMap, obj, {foo: true});
-    expect(result).to.not.equal(obj);
-    expect(result).to.eql({foo: {id: '0xACAB'}, bar: 'test'});
-  });
-});
 */
 
-describe('proxifyObject()', () => {
-  it('should call beforeSet and afterSet when assigning a key');
-  it('should call beforeSet and afterSet when deleting a key');
-  it('should call beforeSet and afterSet when assigning a nested key');
-  it('should call beforeSet and afterSet when deleting a nested key');
-});
+describe('helpers', () => {
+  interface IFooBar {
+    foo?: string;
+    bar?: string;
+  }
 
-describe('setObject', () => {
-  const schemaMap: ISchemaMap = {
+  const schemaMap: SchemaMap<IFooBar> = {
     foo: new StringSchema({required: true, default: 'default'}),
     bar: new StringSchema(),
   };
 
-  let parentInstance: IOctoParentInstance;
+  let parentInstance: ISchemaParentInstance;
 
   beforeEach(() => {
     parentInstance  = {
       value: {},
+      schema: new StringSchema(),
       beforeChange: spy(),
       afterChange: spy(),
     };
   });
 
-  it('should throw if key not in schema', () => {
-    expect(() => setObject({baz: 'foo'}, {}, {}, parentInstance, schemaMap, {}))
-      .to.throw(SanitizationError, 'Key baz not found in schema.');
+  /*
+  describe('populateObject()', () => {
+    const schemaMap = {
+      foo: new ReferenceSchema({collection: () => collection}),
+      bar: new StringSchema(),
+    };
+
+    it('should throw if populateReference is not an object', async () => {
+      await expect(populateObject(schemaMap, {foo: '0xACAB', bar: 'baz'}, true))
+        .to.be.rejectedWith(Error, 'populateReference must be an object.');
+    });
+
+    it('should throw if a key is not populatable', async () => {
+      await expect(populateObject(schemaMap, {foo: '0xACAB', bar: 'baz'}, {bar: true}))
+        .to.be.rejectedWith(Error, 'Key bar is not populatable.');
+    });
+
+    it('should ignore a key if it\'s undefined', async () => {
+      const obj = {bar: 'test'};
+      const result = await populateObject(schemaMap, obj, {foo: true});
+      expect(result).to.not.equal(obj);
+      expect(result).to.eql({bar: 'test'});
+    });
+
+    it('should populate a key', async () => {
+      const obj = {foo: '0xACAB', bar: 'test'};
+      const result = await populateObject(schemaMap, obj, {foo: true});
+      expect(result).to.not.equal(obj);
+      expect(result).to.eql({foo: {id: '0xACAB'}, bar: 'test'});
+    });
+  });
+  */
+
+  describe('proxifyObject()', () => {
+    it('should call beforeSet and afterSet when assigning a key');
+    it('should call beforeSet and afterSet when deleting a key');
+    it('should call beforeSet and afterSet when assigning a nested key');
+    it('should call beforeSet and afterSet when deleting a nested key');
   });
 
-  it('should throw if a nested schema sanitization throws', () => {
-    expect(() => setObject({bar: 42}, {}, {}, parentInstance, schemaMap, {}))
-      .to.throw(SanitizationError, 'Value is not a string.');
+  describe('setObject', () => {
+    it('should throw if key not in schema', () => {
+      expect(() => setObject({baz: 'foo'}, {}, {}, parentInstance, schemaMap, {}))
+        .to.throw(SanitizationError, 'Key baz not found in schema.');
+    });
+
+    it('should throw if a nested schema sanitization throws', () => {
+      expect(() => setObject({bar: 42}, {}, {}, parentInstance, schemaMap, {}))
+        .to.throw(SanitizationError, 'Value is not a string.');
+    });
+
+    it('should do nothing with an empty data object', () => {
+      const obj = {};
+      const octoValueMap: SchemaInstanceMap = {};
+      setObject({}, obj, octoValueMap, parentInstance, schemaMap, {});
+      expect(obj).to.eql({});
+      expect(octoValueMap).to.eql({});
+    });
+
+    it('should set a valid property', () => {
+      const obj = {};
+      const instanceMap: SchemaInstanceMap<IFooBar> = {};
+      setObject({foo: 'bar'}, obj, instanceMap, parentInstance, schemaMap, {});
+      expect(obj).to.eql({foo: 'bar'});
+      expect(instanceMap).to.eql({
+        foo: {value: 'bar', schema: schemaMap.foo, parent: {instance: parentInstance, path: 'foo'}},
+      });
+    });
+
+    it('should not modify an existing property', () => {
+      const obj = {bar: 'baz'};
+      const barInstance = {value: 'baz', schema: schemaMap.bar, parent: {instance: parentInstance, path: 'bar'}};
+      const instanceMap: SchemaInstanceMap<IFooBar> = {bar: barInstance};
+      setObject<IFooBar>({foo: 'bar'}, obj, instanceMap, parentInstance, schemaMap, {});
+      expect(obj).to.eql({foo: 'bar', bar: 'baz'});
+      expect(instanceMap.bar).to.equal(barInstance);
+      expect(instanceMap).to.eql({
+        bar: barInstance,
+        foo: {value: 'bar', schema: schemaMap.foo, parent: {instance: parentInstance, path: 'foo'}},
+      });
+    });
+
+    it('should set a default value', () => {
+      const obj = {};
+      const instanceMap: SchemaInstanceMap<IFooBar> = {};
+      setObject({bar: 'bar'}, obj, instanceMap, parentInstance, schemaMap, {defaults: true});
+      expect(obj).to.eql({foo: 'default', bar: 'bar'});
+      expect(instanceMap).to.eql({
+        bar: {value: 'bar', schema: schemaMap.bar, parent: {instance: parentInstance, path: 'bar'}},
+        foo: {value: 'default', schema: schemaMap.foo, parent: {instance: parentInstance, path: 'foo'}},
+      });
+    });
+
+    it('should replace an object', () => {
+      const obj = {bar: 'baz'};
+      const barInstance = {value: 'baz', schema: schemaMap.bar, parent: {instance: parentInstance, path: 'bar'}};
+      const instanceMap: SchemaInstanceMap<IFooBar> = {bar: barInstance};
+      setObject<IFooBar>({foo: 'bar'}, obj, instanceMap, parentInstance, schemaMap, {replace: true});
+      expect(obj).to.eql({foo: 'bar'});
+      expect(instanceMap).to.eql({
+        foo: {value: 'bar', schema: schemaMap.foo, parent: {instance: parentInstance, path: 'foo'}},
+      });
+    });
   });
 
-  it('should do nothing with an empty data object', () => {
-    const obj = {};
-    const octoValueMap: IOctoValueMap = {};
-    setObject({}, obj, octoValueMap, parentInstance, schemaMap, {});
-    expect(obj).to.eql({});
-    expect(octoValueMap).to.eql({});
+  describe('toObject()', () => {
+    it('should return an object', () => {
+      const instanceMap: SchemaInstanceMap<IFooBar> = {
+        foo: {value: 'bar', schema: schemaMap.foo, parent: {instance: parentInstance, path: 'foo'}},
+      };
+      expect(toObject(instanceMap)).to.eql({foo: 'bar'});
+    });
   });
 
-  it('should set a valid property', () => {
-    const obj = {};
-    const octoValueMap: IOctoValueMap = {};
-    setObject({foo: 'bar'}, obj, octoValueMap, parentInstance, schemaMap, {});
-    expect(obj).to.eql({foo: 'bar'});
-    expect(octoValueMap.foo).to.be.an.instanceOf(OctoString).and
-      .to.have.property('value', 'bar');
-  });
+  describe('validateObject()', () => {
+    const instance: ISchemaParentInstance = {
+      value: {},
+      schema: new StringSchema(),
+      beforeChange: spy(),
+      afterChange: spy(),
+    };
 
-  it('should not modify an existing property', () => {
-    const obj = {bar: 'baz'};
-    const octoString = new OctoString('baz');
-    const octoValueMap: IOctoValueMap = {bar: octoString};
-    setObject({foo: 'bar'}, obj, octoValueMap, parentInstance, schemaMap, {});
-    expect(obj).to.eql({foo: 'bar', bar: 'baz'});
-    expect(octoValueMap.bar).to.equal(octoString);
-    expect(octoValueMap.foo).to.be.an.instanceOf(OctoString).and
-      .to.have.property('value', 'bar');
-  });
+    it('should throw if required key is undefined in the OctoValueMap', async () => {
+      const instanceMap: SchemaInstanceMap<IFooBar> = {};
+      await expect(validateObject(instanceMap, instance, schemaMap))
+        .to.be.rejectedWith(ValidationError, 'Key foo is required.');
+    });
 
-  it('should set a default value', () => {
-    const obj = {};
-    const octoValueMap: IOctoValueMap = {};
-    setObject({bar: 'bar'}, obj, octoValueMap, parentInstance, schemaMap, {defaults: true});
-    expect(obj).to.eql({foo: 'default', bar: 'bar'});
-    expect(octoValueMap.foo).to.be.an.instanceOf(OctoString).and
-      .to.have.property('value', 'default');
-    expect(octoValueMap.bar).to.be.an.instanceOf(OctoString).and
-      .to.have.property('value', 'bar');
-  });
-
-  it('should replace an object', () => {
-    const obj = {bar: 'baz'};
-    const octoValueMap: IOctoValueMap = {bar: new OctoString('baz')};
-    setObject({bar: 'bar'}, obj, octoValueMap, parentInstance, schemaMap, {replace: true});
-    expect(obj).to.eql({bar: 'bar'});
-    expect(octoValueMap.bar).to.be.an.instanceOf(OctoString).and
-      .to.have.property('value', 'bar');
-    expect(octoValueMap).to.not.have.property('foo');
-  });
-});
-
-describe('toObject()', () => {
-  it('should return an object', () => {
-    const octoValueMap: IOctoValueMap = {foo: new OctoString('bar')};
-    expect(toObject(octoValueMap)).to.eql({foo: 'bar'});
-  });
-});
-
-describe('validateObject()', () => {
-  const schemaMap = {foo: new StringSchema({required: true, default: 'default'})};
-  const instance: IOctoParentInstance = {
-    value: {},
-    beforeChange: spy(),
-    afterChange: spy(),
-  };
-
-  it('should throw if required key is undefined in the OctoValueMap', async () => {
-    const octoValueMap: IOctoValueMap = {foo: undefined};
-    await expect(validateObject(octoValueMap, instance, schemaMap))
-      .to.be.rejectedWith(ValidationError, 'Key foo is required.');
-  });
-
-  it('should pass for a valid object', async () => {
-    const octoValueMap: IOctoValueMap = {foo: schemaMap.foo.create('bar')};
-    await validateObject(octoValueMap, instance, schemaMap);
+    it('should pass for a valid object', async () => {
+      const instanceMap: SchemaInstanceMap = {
+        foo: schemaMap.foo.create('bar', {parent: {instance: parentInstance, path: 'foo'}}),
+      };
+      await validateObject(instanceMap, instance, schemaMap);
+    });
   });
 });
 
@@ -173,20 +191,30 @@ describe('OctoObject', () => {
     });
   });
   */
-
+/*
   describe('constructor()', () => {
     // note: the constructor wraps setObject() (see above)
     it('should throw a SanitizationError if value is not an object', () => {
       const schema = new ObjectSchema({schemaMap: {}});
       expect(() => schema.create(42))
-        .to.throw(SanitizationError, 'Data is not an object.');
+        .to.throw(SanitizationError, 'Value is not an object.');
     });
 
-    it('should return an empty object if required', () => {
-      const schema = new ObjectSchema({required: true, schemaMap: {}});
-      const octoObject = schema.create(undefined, {defaults: true});
+    it('should return an empty object for undefined if required', () => {
+      const schema = new ObjectSchema({schemaMap: {}});
+      const octoObject = schema.create(undefined, {});
       expect(octoObject).to.be.an.instanceOf(OctoObject).and
         .have.property('value').that.eql({});
+    });
+
+    it('should return an object for undefined if required and defaults', () => {
+      const schema = new ObjectSchema({
+        required: true,
+        schemaMap: {foo: new StringSchema({default: 'bar'})},
+      });
+      const octoObject = schema.create(undefined, {defaults: true});
+      expect(octoObject).to.be.an.instanceOf(OctoObject).and
+        .have.property('value').that.eql({foo: 'bar'});
     });
 
     it('should return an object', () => {
@@ -233,4 +261,5 @@ describe('OctoObject', () => {
       await schema.create({foo: 'bar'}).validate();
     });
   });
+  */
 });
--- packages/octonom/lib/schema/object.ts
+++ packages/octonom/lib/schema/object.ts
@@ -1,21 +1,27 @@
 import { difference } from 'lodash';
 
 import { SanitizationError, ValidationError } from '../errors';
-import { IOctoInstance, IOctoParentInstance, IOctoValueMap, ISanitizeOptions, ISchema,
-         ISchemaMap, ISchemaOptions, IToObjectOptions,
-         OctoValue, Path, PopulateReference,
-       } from './value';
+import { ISanitizeOptions, ISchema, ISchemaInstance, ISchemaOptions, ISchemaParentInstance,
+         IToObjectOptions, Path, PopulateReference, SchemaInstanceMap, SchemaMap,
+         validate,
+       } from './schema';
+
+export interface IObjectInstance<T extends object> extends ISchemaParentInstance<T> {
+  instanceMap: SchemaInstanceMap<T>;
+}
+export type ObjectInstance<T extends object> = IObjectInstance<T>;
 
 // export interface IArrayOptions<T = any> extends ISchemaOptions<OctoArray<T>> {}
-export interface IObjectOptions<T extends object = object> extends ISchemaOptions<OctoObject<T>> {
-  schemaMap: ISchemaMap;
+export interface IObjectOptions<T extends object = object> extends ISchemaOptions<ObjectInstance<T>> {
+  schemaMap: SchemaMap<T>;
 }
 
+/*
 // populate an object and octoValueMap simultaneously
 export async function populateObject(
   obj: object,
   octoValueMap: IOctoValueMap,
-  schemaMap: ISchemaMap,
+  schemaMap: SchemaMap,
   populateReference: PopulateReference,
 ) {
   const newObj = {};
@@ -40,29 +46,30 @@ export async function populateObject(
 
   return newObj;
 }
+*/
 
 // proxify an object to sync changes to an octoValueMap
-export function proxifyObject(
-  obj: object,
-  octoValueMap: IOctoValueMap,
-  parentInstance: IOctoParentInstance,
-  schemaMap: ISchemaMap,
+export function proxifyObject<T extends object>(
+  obj: T,
+  instanceMap: SchemaInstanceMap<T>,
+  parentInstance: ISchemaParentInstance,
+  schemaMap: SchemaMap<T>,
 ) {
   return new Proxy(obj, {
-    set(target, key, value, receiver) {
+    set(target, key: keyof T, value, receiver) {
       if (typeof key !== 'symbol' && schemaMap[key]) {
         parentInstance.beforeChange([key], value, parentInstance);
 
-        const oldOctoValue = octoValueMap[key];
+        const oldOctoValue = instanceMap[key];
         if (oldOctoValue) {
-          oldOctoValue.detach();
+          delete oldOctoValue.parent;
         }
 
-        octoValueMap[key] = schemaMap[key].create(
+        instanceMap[key] = schemaMap[key].create(
           value,
           {parent: {instance: parentInstance, path: key}},
         );
-        target[key] = octoValueMap[key].value;
+        target[key] = instanceMap[key].value;
 
         parentInstance.afterChange([key], value, parentInstance);
       } else {
@@ -70,16 +77,16 @@ export function proxifyObject(
       }
       return true;
     },
-    deleteProperty(target, key) {
+    deleteProperty(target, key: keyof T) {
       if (typeof key !== 'symbol' && schemaMap[key]) {
         parentInstance.beforeChange([key], undefined, parentInstance);
 
-        const oldOctoValue = octoValueMap[key];
+        const oldOctoValue = instanceMap[key];
         if (oldOctoValue) {
-          oldOctoValue.detach();
+          delete oldOctoValue.parent;
         }
 
-        delete octoValueMap[key];
+        delete instanceMap[key];
         delete target[key];
 
         parentInstance.afterChange([key], undefined, parentInstance);
@@ -88,16 +95,16 @@ export function proxifyObject(
       }
       return true;
     },
-  });
+  }) as T;
 }
 
 // set an object and octoValueMap simultaneously
-export function setObject(
+export function setObject<T extends object>(
   data: object,
-  obj: object,
-  octoValueMap: IOctoValueMap,
-  parentInstance: IOctoParentInstance,
-  schemaMap: ISchemaMap,
+  obj: T,
+  instanceMap: SchemaInstanceMap<T>,
+  parentInstance: ISchemaParentInstance,
+  schemaMap: SchemaMap<T>,
   sanitizeOptions: ISanitizeOptions,
 ) {
   const dataKeys = Object.keys(data);
@@ -109,40 +116,41 @@ export function setObject(
     );
   }
 
-  const newOctoValueMap = {};
+  const newOctoValueMap = {} as SchemaInstanceMap<T>;
 
   // sanitize all values before setting
-  schemaKeys.forEach(key => {
-    if (data[key] === undefined && !sanitizeOptions.defaults) {
+  schemaKeys.forEach((key: keyof T) => {
+    if (data[key as any] === undefined && (!schemaMap[key].options.required || !sanitizeOptions.defaults)) {
       return;
     }
 
     newOctoValueMap[key] = schemaMap[key].create(
-      data[key],
+      data[key as any],
       {...sanitizeOptions, parent: {instance: parentInstance, path: key}},
     );
   });
 
-  schemaKeys.forEach(key => {
+  schemaKeys.forEach((key: keyof T) => {
     if (sanitizeOptions.replace || key in data) {
-      if (octoValueMap[key]) {
-        octoValueMap[key].detach();
+      if (instanceMap[key]) {
+        delete instanceMap[key].parent;
       }
-      delete octoValueMap[key];
+      delete instanceMap[key];
       delete obj[key];
     }
 
     if (newOctoValueMap[key]) {
-      octoValueMap[key] = newOctoValueMap[key];
+      instanceMap[key] = newOctoValueMap[key];
       obj[key] = newOctoValueMap[key].value;
     }
   });
 }
 
-export function toObject(octoValueMap: IOctoValueMap, options?: IToObjectOptions) {
-  const newObj = {};
-  Object.keys(octoValueMap).forEach(key => {
-    const value = octoValueMap[key].toObject();
+export function toObject<T extends object>(instanceMap: SchemaInstanceMap<T>, options?: IToObjectOptions) {
+  const newObj = {} as T;
+  Object.keys(instanceMap).forEach((key: keyof T) => {
+    const instance = instanceMap[key];
+    const value = instance.schema.toObject(instance, options);
     if (value === undefined) {
       return;
     }
@@ -153,74 +161,83 @@ export function toObject(octoValueMap: IOctoValueMap, options?: IToObjectOptions
   return newObj;
 }
 
-export async function validateObject(
-  octoValueMap: IOctoValueMap,
-  parentInstance: IOctoParentInstance,
-  schemaMap: ISchemaMap,
+export async function validateObject<T extends object>(
+  instanceMap: SchemaInstanceMap<T>,
+  parentInstance: ISchemaParentInstance,
+  schemaMap: SchemaMap<T>,
 ) {
-  await Promise.all(Object.keys(schemaMap).map(async key => {
+  await Promise.all(Object.keys(schemaMap).map(async (key: keyof T) => {
     const schema = schemaMap[key];
-    const octoValue = octoValueMap[key];
-    if (schema.options.required && (octoValue === undefined || octoValue.value === undefined)) {
+    const instance = instanceMap[key];
+
+    if (schema.options.required && instance === undefined) {
       throw new ValidationError(`Key ${key} is required.`, 'required', {instance: parentInstance, path: key});
     }
-    await octoValueMap[key].validate();
+
+    if (instance) {
+      await schema.validate(instance);
+    }
   }));
 }
 
-export class OctoObject<T extends object = object> extends OctoValue<T> {
-  public value: T;
-  public octoValueMap: IOctoValueMap;
-
-  constructor(value: any, public schemaOptions: IObjectOptions<T>, sanitizeOptions: ISanitizeOptions) {
-    super(schemaOptions, sanitizeOptions.parent);
-
-    // return undefined if no data and value is not required
-    const newValue = value === undefined && sanitizeOptions.defaults ? {} : value;
-
-    if (typeof newValue !== 'object') {
-      throw new SanitizationError('Data is not an object.', 'no-object', sanitizeOptions.parent);
-    }
-
-    const octoValueMap = {};
-    const obj = {};
-
-    setObject(newValue, obj, octoValueMap, this, schemaOptions.schemaMap, sanitizeOptions);
-
-    this.octoValueMap = octoValueMap;
-    this.value = proxifyObject(obj, this.octoValueMap, this, this.schemaOptions.schemaMap) as T;
-  }
-
-  public beforeChange(path: Path, value: any, oldInstance: IOctoInstance) {
-    if (this.parent && this.parent.instance.beforeChange) {
-      this.parent.instance.beforeChange([this.parent.path].concat(path), value, oldInstance);
-    }
-  }
-
-  public afterChange(path: Path, value: any, newInstance: IOctoInstance) {
-    if (this.parent && this.parent.instance.afterChange) {
-      this.parent.instance.afterChange([this.parent.path].concat(path), value, newInstance);
-    }
-  }
-
-  public async populate(populateReference: PopulateReference) {
-    return populateObject(this.value, this.octoValueMap, this.schemaOptions.schemaMap, populateReference) as Promise<T>;
-  }
-
-  public toObject(options?: IToObjectOptions) {
-    return toObject(this.octoValueMap, options) as T;
-  }
-
-  public async validate() {
-    await validateObject(this.octoValueMap, this, this.schemaOptions.schemaMap);
-    await super.validate();
-  }
-}
-
-export class ObjectSchema<T extends object = object> implements ISchema {
+export class ObjectSchema<T extends object = object> implements ISchema<T, ObjectInstance<T>> {
   constructor(public options: IObjectOptions<T>) {}
 
-  public create(value: any, sanitizeOptions: ISanitizeOptions = {}) {
-    return new OctoObject<T>(value, this.options, sanitizeOptions);
+  public create(value: any, sanitizeOptions: ISanitizeOptions): ObjectInstance<T> {
+    const sanitizedValue = this.sanitize(value, sanitizeOptions);
+
+    const instance: ObjectInstance<T> = {
+      instanceMap: {} as SchemaInstanceMap<T>,
+      parent: sanitizeOptions.parent,
+      schema: this,
+      value: undefined,
+      beforeChange(path: Path, newValue: any, oldInstance: ISchemaInstance) {
+        if (instance.parent) {
+          instance.parent.instance.beforeChange([instance.parent.path].concat(path), newValue, oldInstance);
+        }
+      },
+      afterChange(path: Path, newValue: any, newInstance: ISchemaInstance) {
+        if (instance.parent) {
+          instance.parent.instance.afterChange([instance.parent.path].concat(path), newValue, newInstance);
+        }
+      },
+    };
+
+    const obj = {};
+    setObject(sanitizedValue, obj, instance.instanceMap, instance, this.options.schemaMap, sanitizeOptions);
+
+    instance.value = proxifyObject<T>(obj as T, instance.instanceMap, instance, this.options.schemaMap);
+
+    return instance;
+  }
+
+  // public async populate(populateReference: PopulateReference) {
+  //   return populateObject(
+  //     this.value, this.octoValueMap, this.schemaOptions.schemaMap, populateReference
+  //   ) as Promise<T>;
+  // }
+
+  public toObject(instance: ObjectInstance<T>, options?: IToObjectOptions) {
+    return toObject<T>(instance.instanceMap, options);
+  }
+
+  public async validate(instance: ObjectInstance<T>) {
+    await validateObject(instance.instanceMap, instance, this.options.schemaMap);
+    await validate(this.options, instance);
+  }
+
+  protected sanitize(value: any, sanitizeOptions: ISanitizeOptions) {
+    if (value === undefined) {
+      if (this.options.required && sanitizeOptions.defaults) {
+        return {};
+      }
+      return undefined;
+    }
+
+    if (typeof value !== 'object') {
+      throw new SanitizationError('Value is not an object.', 'no-object', sanitizeOptions.parent);
+    }
+
+    return value;
   }
 }
