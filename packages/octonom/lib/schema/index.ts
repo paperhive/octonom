4f640fd005bbb7b44518c3840b9abe16b2fa1008
André Gaul <andre@gaul.io>
2017-10-05 23:33:01 +0200

    re-enable ModelSchema tests

--- packages/octonom/lib/schema/index.ts
+++ packages/octonom/lib/schema/index.ts
@@ -7,20 +7,20 @@ import { IModelOptions, ModelSchema } from './model';
 // import { INumberOptions, NumberSchema } from './number';
 import { IObjectOptions, ObjectSchema } from './object';
 // import { IReferenceOptions, ReferenceSchema } from './reference';
+import { ISanitizeOptions, ISchema, ISchemaInstance } from './schema';
 import { IStringOptions, StringSchema } from './string';
-import { ISanitizeOptions, ISchema } from './value';
 
-export function getSchemaDecorator(createSchema: () => ISchema): PropertyDecorator {
+export function getSchemaDecorator(createSchema: () => ISchema<any, ISchemaInstance>): PropertyDecorator {
   return (target: IModel, key: string) => target.constructor.setSchema(key, createSchema());
 }
 
 /* tslint:disable:variable-name */
 export const Property = {
   Any: (options: IAnyOptions = {}) => getSchemaDecorator(() => new AnySchema(options)),
-  Array: (options: IArrayOptions) => getSchemaDecorator(() => new ArraySchema(options)),
+  Array: <T> (options: IArrayOptions) => getSchemaDecorator(() => new ArraySchema<T>(options)),
   // Boolean: (options: IBooleanOptions = {}) => getSchemaDecorator(() => new BooleanSchema(options)),
   // Date: (options: IDateOptions = {}) => getSchemaDecorator(() => new DateSchema(options)),
-  Model: (options: IModelOptions) => getSchemaDecorator(() => new ModelSchema(options)),
+  // Model: (options: IModelOptions) => getSchemaDecorator(() => new ModelSchema(options)),
   // Number: (options: INumberOptions = {}) => getSchemaDecorator(() => new NumberSchema(options)),
   Object: (options: IObjectOptions) => getSchemaDecorator(() => new ObjectSchema(options)),
   // Reference: (options: IReferenceOptions) => getSchemaDecorator(() => new ReferenceSchema(options)),
@@ -32,7 +32,7 @@ export const Schema = {
   Array: ArraySchema,
   // Boolean: BooleanSchema,
   // Date: DateSchema,
-  Model: ModelSchema,
+  // Model: ModelSchema,
   // Number: NumberSchema,
   Object: ObjectSchema,
   // Reference: ReferenceSchema,
