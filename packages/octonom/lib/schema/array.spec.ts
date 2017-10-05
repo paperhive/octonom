ed8b84928d30580d03d67a76973021c27b42d4d6
André Gaul <andre@gaul.io>
2017-10-05 14:07:58 +0200

    adapt ArraySchema tests

--- packages/octonom/lib/schema/array.spec.ts
+++ packages/octonom/lib/schema/array.spec.ts
@@ -2,22 +2,27 @@ import { stub } from 'sinon';
 
 import { ArrayCollection } from '../array-collection';
 import { SanitizationError, ValidationError } from '../errors';
-// import { Model } from '../model';
-import { ArraySchema, OctoArray } from './array';
+import { Model } from '../model';
+import { ArrayInstance, ArraySchema } from './array';
 // import { ModelSchema } from './model';
 // import { ReferenceSchema } from './reference';
+import { ISchemaParentInstance } from './schema';
 import { StringSchema } from './string';
-import { OctoValue } from './value';
 
 describe('ArraySchema', () => {
-  /*
   class TestModel extends Model {
     public foo: string;
   }
   TestModel.setSchema('foo', new StringSchema());
-  */
 
   describe('constructor()', () => {
+    it('should be instantiatable with options', () => {
+      const schema = new ArraySchema({elementSchema: new StringSchema()});
+      expect(schema).to.have.property('options').that.eql({elementSchema: {options: {}}});
+    });
+  });
+
+  describe('create()', () => {
     it('should throw a SanitizationError if value is not an array', () => {
       const schema = new ArraySchema({elementSchema: new StringSchema()});
       expect(() => schema.create(42)).to.throw(SanitizationError, 'Value is not an array.');
@@ -28,9 +33,9 @@ describe('ArraySchema', () => {
       expect(() => schema.create([42])).to.throw(SanitizationError, 'Value is not a string.');
     });
 
-    it('should return undefined', () => {
-      const schema = new ArraySchema({elementSchema: new StringSchema()});
-      expect(schema.create(undefined)).to.have.property('value').that.equal(undefined);
+    it('should return empty array if required and undefined', () => {
+      const schema = new ArraySchema({elementSchema: new StringSchema(), required: true});
+      expect(schema.create(undefined, {defaults: true})).to.have.property('value').that.eql([]);
     });
 
     it('should return an array of strings', () => {
@@ -38,115 +43,112 @@ describe('ArraySchema', () => {
       expect(schema.create(['bar'])).to.have.property('value').that.eql(['bar']);
     });
 
-    /* TODO
-    it('should return an array of models with object input', () => {
-      const schema = new ArraySchema({elementSchema: new ModelSchema({model: TestModel})});
-      const array = schema.sanitize([{foo: 'bar'}], ['key'], {} as Model) as Array<Partial<TestModel>>;
-      expect(array).to.be.an('array').and.to.eql([{foo: 'bar'}]);
-      expect(array[0]).to.be.an.instanceOf(TestModel).and.to.eql({foo: 'bar'});
-    });
+    // TODO
+    // it('should return an array of models with object input', () => {
+    //   const schema = new ArraySchema({elementSchema: new ModelSchema({model: TestModel})});
+    //   const array = schema.sanitize([{foo: 'bar'}], ['key'], {} as Model) as Array<Partial<TestModel>>;
+    //   expect(array).to.be.an('array').and.to.eql([{foo: 'bar'}]);
+    //   expect(array[0]).to.be.an.instanceOf(TestModel).and.to.eql({foo: 'bar'});
+    // });
 
-    it('should return an array of models with model input', () => {
-      const schema = new ArraySchema({elementSchema: new ModelSchema({model: TestModel})});
-      const array = [new TestModel({foo: 'bar'})];
-      expect(schema.sanitize(array, ['key'], {} as Model)).to.eql(array);
-    });
-    */
+    // it('should return an array of models with model input', () => {
+    //   const schema = new ArraySchema({elementSchema: new ModelSchema({model: TestModel})});
+    //   const array = [new TestModel({foo: 'bar'})];
+    //   expect(schema.sanitize(array, ['key'], {} as Model)).to.eql(array);
+    // });
 
-    it('should return empty array if required and undefined', () => {
-      const schema = new ArraySchema({elementSchema: new StringSchema(), required: true});
-      expect(schema.create(undefined)).to.have.property('value').that.eql([]);
-    });
-  });
+    describe('proxifyArray()', () => {
+      const schema = new ArraySchema<string>({elementSchema: new StringSchema()});
 
-  describe('value array proxy', () => {
-    const schema = new ArraySchema({elementSchema: new StringSchema()});
-    let octoArray: OctoArray<any>;
-    let beforeChangeStub;
-    let beforeChangeArray;
-    let afterChangeStub;
-    let afterChangeArray;
+      let beforeChangeStub;
+      let beforeChangeArray: string[];
+      let afterChangeStub;
+      let afterChangeArray: string[];
 
-    beforeEach(() => {
-      octoArray = schema.create(['foo', 'bar', 'bla']);
-      beforeChangeStub = stub(octoArray, 'beforeChange')
-        .callsFake((path, value, oldOctoValue) => beforeChangeArray = oldOctoValue.toObject());
-      afterChangeStub = stub(octoArray, 'afterChange')
-        .callsFake((path, value, newOctoValue) => afterChangeArray = newOctoValue.toObject());
-    });
+      let arrayInstance: ArrayInstance<string>;
 
-    function testArrayProxy(arrayBefore, arrayAfter) {
-      expect(beforeChangeStub).to.be.calledOnce.and.calledWith([], arrayAfter, octoArray);
-      expect(beforeChangeArray).to.eql(arrayBefore);
-      expect(octoArray.value).to.eql(arrayAfter);
-      expect(octoArray.octoValues.map(el => el.value)).to.eql(arrayAfter);
-      octoArray.octoValues.forEach((el, index) => {
-        expect(el.parent).to.eql({instance: octoArray, path: index});
+      beforeEach(() => {
+        arrayInstance = schema.create(['foo', 'bar', 'bla']);
+        beforeChangeStub = stub(arrayInstance, 'beforeChange').callsFake(
+          (path, value, oldInstance) => beforeChangeArray = schema.toObject(oldInstance),
+        );
+        afterChangeStub = stub(arrayInstance, 'afterChange').callsFake(
+          (path, value, newInstance) => afterChangeArray = schema.toObject(newInstance),
+        );
       });
-      expect(afterChangeStub).to.be.calledOnce.and.calledWith([], arrayAfter, octoArray);
-      expect(afterChangeArray).to.eql(arrayAfter);
-    }
 
-    it('should be an array', () => {
-      expect(octoArray.value).to.be.an('array').and.an.instanceOf(Array);
-    });
+      function testArrayProxy(arrayBefore, arrayAfter) {
+        expect(beforeChangeStub).to.be.calledOnce.and.calledWith([], arrayAfter, arrayInstance);
+        expect(beforeChangeArray).to.eql(arrayBefore);
+        expect(arrayInstance.value).to.eql(arrayAfter);
+        expect(arrayInstance.instanceArray.map(el => el.value)).to.eql(arrayAfter);
+        arrayInstance.instanceArray.forEach((instance, index) => {
+          expect(instance.parent).to.eql({instance: arrayInstance, path: index});
+        });
+        expect(afterChangeStub).to.be.calledOnce.and.calledWith([], arrayAfter, arrayInstance);
+        expect(afterChangeArray).to.eql(arrayAfter);
+      }
 
-    it('should intercept pop()', () => {
-      const octoValue = octoArray.octoValues[octoArray.octoValues.length - 1];
-      expect(octoArray.value.pop()).to.equal('bla');
-      testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bar']);
-      expect(octoValue).to.not.have.property('parent');
-    });
+      it('should be an array', () => {
+        expect(arrayInstance.value).to.be.an('array').and.an.instanceOf(Array);
+      });
 
-    it('should intercept push()', () => {
-      expect(octoArray.value.push('baz', 'boo')).to.equal(5);
-      testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bar', 'bla', 'baz', 'boo']);
-    });
+      it('should intercept pop()', () => {
+        const instance = arrayInstance.instanceArray[arrayInstance.instanceArray.length - 1];
+        expect(arrayInstance.value.pop()).to.equal('bla');
+        testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bar']);
+        expect(instance).to.not.have.property('parent');
+      });
 
-    it('should intercept reverse()', () => {
-      const array = octoArray.value;
-      expect(octoArray.value.reverse()).to.equal(array);
-      testArrayProxy(['foo', 'bar', 'bla'], ['bla', 'bar', 'foo']);
-    });
+      it('should intercept push()', () => {
+        expect(arrayInstance.value.push('baz', 'boo')).to.equal(5);
+        testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bar', 'bla', 'baz', 'boo']);
+      });
 
-    it('should intercept sort()', () => {
-      const array = octoArray.value;
-      expect(octoArray.value.sort()).to.equal(array);
-      testArrayProxy(['foo', 'bar', 'bla'], ['bar', 'bla', 'foo']);
-    });
+      it('should intercept reverse()', () => {
+        const array = arrayInstance.value;
+        expect(arrayInstance.value.reverse()).to.equal(array);
+        testArrayProxy(['foo', 'bar', 'bla'], ['bla', 'bar', 'foo']);
+      });
 
-    it('should intercept sort() with custom compare function', () => {
-      const array = octoArray.value;
-      expect(octoArray.value.sort((a, b) => a < b ? 1 : -1)).to.equal(array);
-      testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bla', 'bar']);
-    });
+      it('should intercept sort()', () => {
+        const array = arrayInstance.value;
+        expect(arrayInstance.value.sort()).to.equal(array);
+        testArrayProxy(['foo', 'bar', 'bla'], ['bar', 'bla', 'foo']);
+      });
 
-    it('should intercept splice(start)', () => {
-      const array = octoArray.value;
-      expect(octoArray.value.splice(2)).to.eql(['bla']);
-      testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bar']);
-    });
+      it('should intercept sort() with custom compare function', () => {
+        const array = arrayInstance.value;
+        expect(arrayInstance.value.sort((a, b) => a < b ? 1 : -1)).to.equal(array);
+        testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bla', 'bar']);
+      });
 
-    it('should intercept splice(start, deleteCount)', () => {
-      const array = octoArray.value;
-      expect(octoArray.value.splice(1, 1)).to.eql(['bar']);
-      testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bla']);
-    });
+      it('should intercept splice(start)', () => {
+        const array = arrayInstance.value;
+        expect(arrayInstance.value.splice(2)).to.eql(['bla']);
+        testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bar']);
+      });
 
-    it('should intercept splice(start, deleteCount, ...elements)', () => {
-      const array = octoArray.value;
-      expect(octoArray.value.splice(1, 1, 'boo', 'baz')).to.eql(['bar']);
-      testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'boo', 'baz', 'bla']);
-    });
+      it('should intercept splice(start, deleteCount)', () => {
+        const array = arrayInstance.value;
+        expect(arrayInstance.value.splice(1, 1)).to.eql(['bar']);
+        testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'bla']);
+      });
 
-    it('should intercept unshift()', () => {
-      const array = octoArray.value;
-      expect(octoArray.value.unshift('boo', 'baz')).to.equal(5);
-      testArrayProxy(['foo', 'bar', 'bla'], ['boo', 'baz', 'foo', 'bar', 'bla']);
+      it('should intercept splice(start, deleteCount, ...elements)', () => {
+        const array = arrayInstance.value;
+        expect(arrayInstance.value.splice(1, 1, 'boo', 'baz')).to.eql(['bar']);
+        testArrayProxy(['foo', 'bar', 'bla'], ['foo', 'boo', 'baz', 'bla']);
+      });
+
+      it('should intercept unshift()', () => {
+        const array = arrayInstance.value;
+        expect(arrayInstance.value.unshift('boo', 'baz')).to.equal(5);
+        testArrayProxy(['foo', 'bar', 'bla'], ['boo', 'baz', 'foo', 'bar', 'bla']);
+      });
     });
   });
-
-  /* TODO
+/*
   describe('populate()', () => {
     class ReferenceModel extends Model {
       public id: string;
@@ -198,65 +200,49 @@ describe('ArraySchema', () => {
     it('should return a new array', () => {
       const schema = new ArraySchema({elementSchema: new StringSchema()});
       const array = ['foo'];
-      const result = schema.create(array).toObject();
+      const result = schema.toObject(schema.create(array));
       expect(result).to.not.equal(array);
       expect(result).to.eql(array);
     });
   });
 
   describe('validate()', () => {
-    // TODO: remove this test? undefined is not possible if required: true
-    it('should throw a ValidationError if required but undefined', async () => {
-      const schema = new ArraySchema({elementSchema: new StringSchema(), required: true});
-      const array = schema.create(undefined);
-      delete array.value;
-      delete array.octoValues;
-      await expect(array.validate())
-        .to.be.rejectedWith(ValidationError, 'Required value is undefined.');
-    });
-
     it('should throw if value has less than min elements', async () => {
       const schema = new ArraySchema({elementSchema: new StringSchema(), minLength: 2});
-      await expect(schema.create(['foo']).validate())
+      await expect(schema.validate(schema.create(['foo'])))
         .to.be.rejectedWith(ValidationError, 'Array must have at least 2 elements.');
     });
 
     it('should throw if value has more than max elements', async () => {
       const schema = new ArraySchema({elementSchema: new StringSchema(), maxLength: 2});
-      await expect(schema.create(['foo', 'bar', 'baz']).validate())
+      await expect(schema.validate(schema.create(['foo', 'bar', 'baz'])))
         .to.be.rejectedWith(ValidationError, 'Array must have at most 2 elements.');
     });
 
     it('should run custom validator', async () => {
-      const schema = new ArraySchema({
+      const schema = new ArraySchema<string>({
         elementSchema: new StringSchema(),
-        validate: async (octoArray: OctoArray) => {
-          if (octoArray.value.indexOf('baz') !== -1) {
+        validate: async (instance: ArrayInstance<string>) => {
+          if (instance.value.indexOf('baz') !== -1) {
             throw new ValidationError('baz is not allowed.');
           }
         },
       });
-      await schema.create(['foo']).validate();
-      await expect(schema.create(['foo', 'bar', 'baz']).validate())
+      await schema.validate(schema.create(['foo']));
+      await expect(schema.validate(schema.create(['foo', 'bar', 'baz'])))
         .to.be.rejectedWith(ValidationError, 'baz is not allowed.');
-    });
-
-    it('should validate undefined', async () => {
-      const schema = new ArraySchema({elementSchema: new StringSchema()});
-      await schema.create(undefined).validate();
     });
 
     it('should validate an array of strings', async () => {
       const schema = new ArraySchema({elementSchema: new StringSchema()});
-      await schema.create(['foo']).validate();
+      await schema.validate(schema.create(['foo']));
     });
 
-    /* TODO
-    it('should validate an array of models', async () => {
-      const schema = new ArraySchema({elementSchema: new ModelSchema({model: TestModel})});
-      const array = [new TestModel({foo: 'bar'})];
-      await schema.validate(array, ['key'], {} as Model);
-    });
-    */
+    // TODO
+    // it('should validate an array of models', async () => {
+    //   const schema = new ArraySchema({elementSchema: new ModelSchema({model: TestModel})});
+    //   const array = [new TestModel({foo: 'bar'})];
+    //   await schema.validate(array, ['key'], {} as Model);
+    // });
   });
 });
