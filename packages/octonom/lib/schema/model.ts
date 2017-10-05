51fe094d007d8e3920368730a65cc9ea7f0190ab
André Gaul <andre@gaul.io>
2017-10-05 19:08:55 +0200

    adapt ModelSchema to new schema

--- packages/octonom/lib/schema/model.ts
+++ packages/octonom/lib/schema/model.ts
@@ -1,18 +1,48 @@
 import { SanitizationError, ValidationError } from '../errors';
 import { IModelConstructor, Model } from '../model';
-import { ISanitizeOptions, ISchema, ISchemaOptions, IToObjectOptions,
-         OctoValue, Path, PopulateReference,
-       } from './value';
+import { ObjectInstance } from './object';
+import { ISanitizeOptions, ISchema, ISchemaInstance, ISchemaOptions,
+         ISchemaParentInstance, IToObjectOptions,
+         Path, PopulateReference, validate,
+       } from './schema';
 
-export interface IModelOptions<T extends Model = Model> extends ISchemaOptions<OctoModel<T>> {
+export type ModelInstance<T extends Model = Model> = ISchemaParentInstance<T>;
+
+export interface IModelOptions<T extends Model = Model> extends ISchemaOptions<ModelInstance<T>> {
   model: IModelConstructor<T>;
+  callParentHooks?: boolean;
 }
 
-export class OctoModel<T extends Model = Model> extends OctoValue<T> {
-  constructor(value: any, public schemaOptions: IModelOptions<T>, sanitizeOptions: ISanitizeOptions) {
-    super(value, schemaOptions, sanitizeOptions);
+export class ModelSchema<T extends Model = Model> implements ISchema<T, ModelInstance<T>, Partial<T>> {
+  constructor(public options: IModelOptions<T>) {}
+
+  public create(value: any, sanitizeOptions: ISanitizeOptions = {}) {
+    if (value === undefined && !this.options.required) {
+      return undefined;
+    }
+
+    const instance: ModelInstance<T> = {
+      parent: sanitizeOptions.parent,
+      schema: this,
+      value: undefined,
+      beforeChange: (path: Path, newValue: any, oldInstance: ISchemaInstance) => {
+        if (this.options.callParentHooks !== false && instance.parent) {
+          instance.parent.instance.beforeChange([instance.parent.path].concat(path), newValue, oldInstance);
+        }
+      },
+      afterChange: (path: Path, newValue: any, newInstance: ISchemaInstance) => {
+        if (this.options.callParentHooks !== false && instance.parent) {
+          instance.parent.instance.afterChange([instance.parent.path].concat(path), newValue, newInstance);
+        }
+      },
+    };
+
+    instance.value = new this.options.model(value || {}, sanitizeOptions);
+
+    return instance;
   }
 
+  /*
   public async populate(populateReference: PopulateReference) {
     if (typeof populateReference !== 'object') {
       throw new Error('populateReference must be an object.');
@@ -20,38 +50,13 @@ export class OctoModel<T extends Model = Model> extends OctoValue<T> {
 
     return this.value.populate(populateReference);
   }
-
-  public sanitize(value: any, sanitizeOptions: ISanitizeOptions = {}) {
-    if (value === undefined && !this.schemaOptions.required) {
-      return undefined;
-    }
-
-    // create new instance
-    return new this.schemaOptions.model(value || {}, sanitizeOptions);
+  */
+  public toObject(instance: ModelInstance<T>, options?: IToObjectOptions) {
+    return instance.value.toObject();
   }
 
-  public toObject(options?: IToObjectOptions) {
-    return this.value.toObject(options) as T;
-  }
-
-  public async validate() {
-    if (this.value === undefined) {
-      if (this.schemaOptions.required) {
-        throw new ValidationError('Required value is undefined.', 'required', this);
-      }
-      return;
-    }
-
-    await this.value.validate();
-
-    await super.validate();
-  }
-}
-
-export class ModelSchema<T extends Model = Model> implements ISchema {
-  constructor(public options: IModelOptions<T>) {}
-
-  public create(value: any, sanitizeOptions: ISanitizeOptions = {}) {
-    return new OctoModel<T>(value, this.options, sanitizeOptions);
+  public async validate(instance: ModelInstance<T>) {
+    await instance.value.validate();
+    await validate(this.options, instance);
   }
 }
