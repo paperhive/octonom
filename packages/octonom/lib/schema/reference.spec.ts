6d02ae053a14748a59c532083e02a9aa5aa2144b
André Gaul <andre@gaul.io>
2017-10-07 18:17:16 +0200

    reference: adapt ReferenceSchema

--- packages/octonom/lib/schema/reference.spec.ts
+++ packages/octonom/lib/schema/reference.spec.ts
@@ -8,9 +8,44 @@ describe('ReferenceSchema', () => {
   class TestModel extends Model {
     public id: string;
   }
-  TestModel.setSchema('id', new StringSchema());
+  TestModel.setSchema('id', new StringSchema({required: true}));
 
   const collection = () => new ArrayCollection<TestModel>(TestModel, {modelIdField: 'id'});
+
+  describe('constructor()', () => {
+    it('should be instantiatable with options', () => {
+      const schema = new ReferenceSchema({collection});
+      expect(schema).to.have.property('options').that.eql({collection});
+    });
+  });
+
+  describe('create()', () => {
+    const schema = new ReferenceSchema({collection});
+
+    it('should throw a SanitizationError if value is not a string or model', () => {
+      expect(() => schema.create(42))
+        .to.throw(SanitizationError, 'Value is not an instance or an id.');
+    });
+
+    it('should return undefined', () => {
+      expect(schema.create(undefined)).to.eql(undefined);
+    });
+
+    it('should return a string', () => {
+      expect(schema.create('id')).to.contain({id: 'id', value: 'id'});
+    });
+
+    it('should return a model instance ', () => {
+      const modelInstance  = new TestModel({id: 'id'});
+      const instance = schema.create(modelInstance);
+      expect(instance).to.eql({
+        id: 'id',
+        value: {id: 'id'},
+        schema,
+      });
+      expect(instance.value).to.be.instanceOf(TestModel);
+    });
+  });
 
   describe('populate()', () => {
     class FriendModel extends Model {
@@ -31,50 +66,43 @@ describe('ReferenceSchema', () => {
     friendCollection.insert(bob);
 
     it('should throw if instance cannot be found', async () => {
-      await expect(schema.populate('karl', true))
-        .to.be.rejectedWith(PopulationError, 'Instance with id karl not found.');
+      const instance = schema.create('karl');
+      const value = await schema.populate(instance, true);
+      expect(value).to.equal(undefined);
+      expect(instance.value).to.equal(value);
+      expect(instance.id).to.equal('karl');
     });
 
     it('should return an instance if id is provided', async () => {
-      const instance = await schema.populate('alice', true);
-      expect(instance).to.be.an.instanceOf(FriendModel).and.to.eql({id: 'alice', friend: 'bob'});
+      const instance = schema.create('alice');
+      const value = await schema.populate(instance, true);
+      expect(value).to.eql(alice);
+      expect(instance.value).to.equal(value);
+      expect(instance.id).to.equal('alice');
     });
 
     it('should return an instance if instance is provided', async () => {
-      const instance = await schema.populate(alice, true);
-      expect(instance).to.equal(alice);
+      const instance = schema.create(alice);
+      const value = await schema.populate(instance, true);
+      expect(value).to.eql(alice);
+      expect(instance.value).to.equal(value);
+      expect(instance.id).to.equal('alice');
     });
 
     it('should return a deep populated instance if id is provided', async () => {
-      const instance = await schema.populate('alice', {friend: true});
-      expect(instance).to.be.an.instanceOf(FriendModel).and.to.eql({id: 'alice', friend: {id: 'bob'}});
+      const instance = schema.create('alice');
+      const value = await schema.populate(instance, {friend: true});
+      expect(value).to.eql({...alice, friend: bob});
+      expect(instance.value).to.equal(value);
+      expect(instance.id).to.equal('alice');
     });
 
     it('should return a deep populated instance if instance is provided', async () => {
-      const instance = await schema.populate(alice, {friend: true});
-      expect(instance).to.equal(alice).and.to.eql({id: 'alice', friend: {id: 'bob'}});
-    });
-  });
-
-  describe('sanitize()', () => {
-    const schema = new ReferenceSchema({collection});
-
-    it('should throw a SanitizationError if value is not a string or model', () => {
-      expect(() => schema.sanitize(42, ['key'], {} as Model))
-        .to.throw(SanitizationError, 'Value is not an instance or an id.');
-    });
-
-    it('should return undefined', () => {
-      expect(schema.sanitize(undefined, ['key'], {} as Model)).to.eql(undefined);
-    });
-
-    it('should return a string', () => {
-      expect(schema.sanitize('id', ['key'], {} as Model)).to.eql('id');
-    });
-
-    it('should return a model instance ', () => {
-      const instance  = new TestModel();
-      expect(schema.sanitize(instance, ['key'], {} as Model)).to.eql(instance);
+      const instance = schema.create(alice);
+      const value = await schema.populate(instance, {friend: true});
+      expect(value).to.eql({...alice, friend: bob});
+      expect(instance.value).to.equal(value);
+      expect(instance.id).to.equal('alice');
     });
   });
 
@@ -82,56 +110,49 @@ describe('ReferenceSchema', () => {
     const schema = new ReferenceSchema({collection});
 
     it('should return an id', () => {
-      expect(schema.toObject('id')).to.equal('id');
+      expect(schema.toObject(schema.create('id'))).to.equal('id');
     });
 
     it('should return an object for a populated reference', () => {
-      expect(schema.toObject(new TestModel({id: '0xACAB'})))
+      expect(schema.toObject(schema.create(new TestModel({id: '0xACAB'}))))
         .to.eql({id: '0xACAB'});
     });
 
     it('should return an id for a populated reference with unpopulate', () => {
-      expect(schema.toObject(new TestModel({id: '0xACAB'}), {unpopulate: true}))
+      expect(schema.toObject(schema.create(new TestModel({id: '0xACAB'})), {unpopulate: true}))
         .to.eql('0xACAB');
     });
   });
 
   describe('validate()', () => {
-    it('should throw a ValidationError if required but undefined', async () => {
-      const schema = new ReferenceSchema({collection, required: true});
-      await expect(schema.validate(undefined, ['key'], {} as Model))
-        .to.be.rejectedWith(ValidationError, 'Required value is undefined.');
-    });
-
-    it('should throw a SanitizationError if value is not a string or model', async () => {
-      const schema = new ReferenceSchema({collection});
-      await expect(schema.validate(42 as any, ['key'], {} as Model))
-        .to.be.rejectedWith(ValidationError, 'Value is not an id or TestModel instance.');
-    });
-
     it('should run custom validator', async () => {
       const schema = new ReferenceSchema({
         collection,
-        validate: async value => {
-          if (value === 'foo') {
+        validate: async instance => {
+          if (instance.value === 'foo') {
             throw new ValidationError('foo is not allowed.');
           }
         },
       });
-      await schema.validate('bar', ['key'], {} as Model);
-      await schema.validate(new TestModel(), ['key'], {} as Model);
-      await expect(schema.validate('foo', ['key'], {} as Model))
+      await schema.validate(schema.create('bar'));
+      await schema.validate(schema.create(new TestModel({id: '42'})));
+      await expect(schema.validate(schema.create('foo')))
         .to.be.rejectedWith(ValidationError, 'foo is not allowed.');
     });
 
-    it('should validate undefined', async () => {
+    it('should not validate a populated model', async () => {
       const schema = new ReferenceSchema({collection});
-      await schema.validate(undefined, ['key'], {} as Model);
+      const instance = schema.create(new TestModel({id: '42'}));
+      const modelInstance = instance.value as TestModel;
+      delete modelInstance.id;
+      await expect(modelInstance.validate())
+        .to.be.rejectedWith(ValidationError, 'Key id is required.');
+      await schema.validate(instance);
     });
 
     it('should validate a string', async () => {
       const schema = new ReferenceSchema({collection});
-      await schema.validate('foo', ['key'], {} as Model);
+      await schema.validate(schema.create('foo'));
     });
   });
 });
--- packages/octonom/lib/schema/reference.ts
+++ packages/octonom/lib/schema/reference.ts
@@ -1,78 +1,88 @@
 import { Collection } from '../collection';
 import { PopulationError, SanitizationError, ValidationError } from '../errors';
 import { Model } from '../model';
-import { ISanitizeOptions, ISchema, ISchemaOptions, IToObjectOptions,
-         Path, PopulateReference, runValidator,
+import { ISanitizeOptions, ISchema, ISchemaInstance, ISchemaOptions, IToObjectOptions,
+         Path, PopulateReference, validate,
        } from './schema';
 
-export interface IReferenceOptions<TModel extends Model = Model> extends ISchemaOptions<string | TModel> {
+export interface IReferenceInstance<TModel extends Model = Model> extends ISchemaInstance<TModel | string> {
+  id: string;
+}
+export type ReferenceInstance<TModel extends Model = Model> = IReferenceInstance<TModel>;
+
+export interface IReferenceOptions<TModel extends Model = Model> extends ISchemaOptions<ReferenceInstance<TModel>> {
   collection: () => Collection<TModel>;
 }
 
-export class ReferenceSchema<TModel extends Model = Model> implements ISchema<Model, TModel> {
-  constructor(public options: IReferenceOptions) {}
+export class ReferenceSchema<TModel extends Model = Model> implements
+    ISchema<TModel | string, ReferenceInstance<TModel>, Partial<TModel> | string> {
+  constructor(public options: IReferenceOptions<TModel>) {}
 
-  public async populate(value: string | Model, populateReference: PopulateReference) {
-    let instance: Model;
-    if (typeof value === 'string') {
-      instance = await this.options.collection().findById(value);
+  public create(value: any, sanitizeOptions: ISanitizeOptions = {}): ReferenceInstance<TModel> {
+    const sanitizedValue = this.sanitize(value, sanitizeOptions);
 
-      if (instance === undefined) {
-        throw new PopulationError(`Instance with id ${value} not found.`);
-      }
-    } else {
-      instance = value;
+    if (sanitizedValue === undefined) {
+      return undefined;
     }
 
-    if (populateReference !== true) {
-      await instance.populate(populateReference);
+    const idField = this.options.collection().modelIdField;
+    const id = typeof sanitizedValue === 'string'
+      ? sanitizedValue
+      : sanitizedValue[idField];
+
+    if (id === undefined) {
+      throw new SanitizationError('Reference id is undefined.', 'no-id', sanitizeOptions.parent);
     }
+
+    const instance: ReferenceInstance<TModel> = {
+      value: sanitizedValue,
+      id,
+      schema: this,
+    };
 
     return instance;
   }
 
-  public sanitize(value: any, path: Path, instance: TModel, options: ISanitizeOptions = {}) {
-    if (value === undefined) {
-      return undefined;
+  public async populate(instance: ReferenceInstance<TModel>, populateReference: PopulateReference) {
+    const modelInstance = await this.options.collection().findById(instance.id);
+
+    if (populateReference !== true) {
+      await modelInstance.populate(populateReference);
     }
 
-    // valid data?
-    if (!(value instanceof this.options.collection().model) && !(typeof value === 'string')) {
+    instance.value = modelInstance;
+
+    return instance.value;
+  }
+
+  public toObject(instance: ReferenceInstance<TModel>, options: IToObjectOptions = {}): Partial<TModel> | string {
+    if (typeof instance.value === 'string') {
+      return instance.value;
+    }
+    return options.unpopulate
+      ? instance.value[this.options.collection().modelIdField]
+      : instance.value.toObject(options);
+  }
+
+  public async validate(instance: ReferenceInstance<TModel>) {
+    await validate(this.options, instance);
+  }
+
+  protected sanitize(value: any, sanitizeOptions: ISanitizeOptions = {}): TModel | string {
+    if (value === undefined || typeof value === 'string') {
+      return value;
+    }
+
+    // If it's not an id we expect a model. We don't instantiate from
+    // a plain object because an object coming from a database will only
+    // contain the id and if it has been populated it will already be a model.
+    const model = this.options.collection().model;
+    if (!(value instanceof model)) {
       throw new SanitizationError('Value is not an instance or an id.');
     }
 
-    return value;
+    // clone model instance
+    return new model(value, sanitizeOptions);
   }
 
-  public toObject(value: string | Model, options: IToObjectOptions = {}) {
-    if (typeof value === 'string') {
-      return value;
-    }
-    return options.unpopulate
-      ? value[this.options.collection().modelIdField]
-      : value.toObject(options);
-  }
-
-  public async validate(value: string | Model, path: Path, instance: TModel) {
-    if (value === undefined) {
-      if (this.options.required) {
-        throw new ValidationError('Required value is undefined.', 'required', value, path, instance);
-      }
-      return;
-    }
-
-    // note: we do not run validation on a populated reference since it's not part of
-    //       the model instance
-    const collection = this.options.collection();
-    if (typeof value !== 'string' && !(value instanceof collection.model)) {
-      throw new ValidationError(
-        `Value is not an id or ${collection.model.name} instance.`,
-        'no-number', value, path, instance,
-      );
-    }
-
-    if (this.options.validate) {
-      await runValidator(this.options.validate, value, path, instance);
-    }
-  }
 }
