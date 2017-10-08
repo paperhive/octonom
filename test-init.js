8a9541c7a25b6aaad1d727c510227a0b257cd1c0
André Gaul <andre@gaul.io>
2017-10-08 21:01:00 +0200

    package: add chai-subset

--- test-init.js
+++ test-init.js
@@ -1,9 +1,11 @@
 require('ts-node').register();
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const chaiSubset = require('chai-subset');
 const sinonChai = require('sinon-chai');
 
 chai.use(chaiAsPromised);
+chai.use(chaiSubset);
 chai.use(sinonChai);
 
 // expose global variable 'expect'
