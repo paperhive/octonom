1f365c79d0ccdfba39e3e186f490701901f6ad19
André Gaul <andre@gaul.io>
2017-10-16 10:32:41 +0200

    timestamp: test CreatedAt and UpdatedAt

--- packages/octonom-timestamp/lib/timestamp.spec.ts
+++ packages/octonom-timestamp/lib/timestamp.spec.ts
@@ -1,9 +1,76 @@
 import { expect } from 'chai';
 import { Model, Property } from 'octonom';
 
-import { Timestamp } from './timestamp';
+import { CreatedAt, Timestamp, UpdatedAt } from './timestamp';
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+class Timestamped extends Model {
+  @CreatedAt()
+  public createdAt: Date;
+
+  @UpdatedAt()
+  public updatedAt: Date;
+
+  @Property.String()
+  public foo: string;
+}
+
+describe('CreatedAt', () => {
+  it('should set createdAt on a new instance', () => {
+    const instance = new Timestamped();
+    expect(instance).to.have.property('createdAt').which.is.an.instanceOf(Date);
+  });
+
+  it('should set createdAt on an initialized new instance', () => {
+    const instance = new Timestamped({foo: 'bar'});
+    expect(instance).to.have.property('createdAt').which.is.an.instanceOf(Date);
+  });
+
+  it('should use createdAt from initialization data', () => {
+    const createdAt = new Date('2016-09-15T20:00:00.000Z');
+    const instance = new Timestamped({createdAt, foo: 'bar'});
+    expect(instance).to.have.property('createdAt').which.is.an.instanceOf(Date);
+    expect(instance.createdAt.getTime()).to.equal(createdAt.getTime());
+  });
+
+  it('should not change when setting a property', async () => {
+    const instance = new Timestamped({foo: 'bar'});
+    expect(instance).to.have.property('createdAt').which.is.an.instanceOf(Date);
+    const createdAt = new Date(instance.createdAt);
+    await sleep(10);
+    instance.foo = 'baz';
+    expect(instance.createdAt.getTime()).to.equal(createdAt.getTime());
+  });
+});
+
+describe('UpdatedAt', () => {
+  it('should set updatedAt on a new instance', () => {
+    const instance = new Timestamped();
+    expect(instance).to.have.property('updatedAt').which.is.an.instanceOf(Date);
+  });
+
+  it('should set updatedAt on an initialized new instance', () => {
+    const instance = new Timestamped({foo: 'bar'});
+    expect(instance).to.have.property('updatedAt').which.is.an.instanceOf(Date);
+  });
+
+  it('should use updatedAt from initialization data', () => {
+    const updatedAt = new Date('2016-09-15T20:00:00.000Z');
+    const instance = new Timestamped({updatedAt, foo: 'bar'});
+    expect(instance).to.have.property('updatedAt').which.is.an.instanceOf(Date);
+    expect(instance.updatedAt.getTime()).to.equal(updatedAt.getTime());
+  });
+
+  it('should update when setting a property', async () => {
+    const instance = new Timestamped({foo: 'bar'});
+    expect(instance).to.have.property('updatedAt').which.is.an.instanceOf(Date);
+    const updatedAt = new Date(instance.updatedAt);
+    await sleep(10);
+    instance.foo = 'baz';
+    expect(updatedAt.getTime()).to.be.lessThan(instance.updatedAt.getTime());
+  });
+});
 
 describe('Timestamp mixin', () => {
   class Base extends Model {
@@ -27,6 +94,7 @@ describe('Timestamp mixin', () => {
       const createdAt = new Date('2016-09-15T20:00:00.000Z');
       const updatedAt = new Date('2016-09-16T07:00:00.000Z');
       const instance = new timestampedClass({name: 'test', createdAt, updatedAt});
+      testInstance(instance);
       expect(instance).to.eql({name: 'test', createdAt, updatedAt});
     });
 
@@ -34,6 +102,7 @@ describe('Timestamp mixin', () => {
       const instance = new timestampedClass();
       await sleep(10);
       instance.set({name: 'test'});
+      testInstance(instance);
       expect(instance.createdAt.getTime()).to.be.lessThan(instance.updatedAt.getTime());
     });
 
@@ -41,6 +110,7 @@ describe('Timestamp mixin', () => {
       const instance = new timestampedClass();
       await sleep(10);
       instance.name = 'test';
+      testInstance(instance);
       expect(instance.createdAt.getTime()).to.be.lessThan(instance.updatedAt.getTime());
     });
 
@@ -48,6 +118,7 @@ describe('Timestamp mixin', () => {
       const instance = new timestampedClass({name: 'test'});
       await sleep(10);
       delete instance.name;
+      testInstance(instance);
       expect(instance.createdAt.getTime()).to.be.lessThan(instance.updatedAt.getTime());
     });
   }
